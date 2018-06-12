81fca1746c4458a705b21a8eb7e72df67183a2db
zhanghongsen <1422432@qq.com>
2018-06-12 15:31:16 +0800

    1
    1

---
+++ 产品大类.sql
@@ -0,0 +1,248 @@
+
+Insert into CMS_PRD_CAT (ID,OU,STATUS,CRD_CD,CRD_NM,IMG_PC,IMG_APP,IMG_PC_DETAIL,IMG_APP_DETAIL,DETAILS_PC,DETAILS_APP,DESCRIPTION,RELEASED_BY,RELEASED_COMMENTS,PRIORITY,USES,USES_DEFAULT,COND_APP,COND_PC,PROC_APP,PROC_PC,FTR_APP,FTR_PC,CREATORS_ID,CREATE_TIMESTAMP,MODIFIERS_ID,MODIFY_TIMESTAMP,AMOUNT,CTS,SORT) values (100001,'999','4',null,'致富通','/2017-05/23/d529276e90de4c9bb640ff2d2058af92.jpg','/2017-05/23/11fa46c22d3a4fada201ce3b0ec08eb9.jpg','/2017-05/23/1d44b3f9dc234e68a32592142d0a0c29.jpg','/2017-05/23/882b895b43d94cbd952f15e65de7c426.jpg','<span style="font-size:16px;"> “致富通”农户贷款系列是指农村信用社（农村商业银行）以评定辖区内农户信用等级为基础，向符合条件的农户发放的用于生产经营、生活消费等用途的贷款。<br />
+农户是指长期(一年以上)居住在乡镇和城关镇所辖行政村的住户和户口不在本地而在本地居住一年以上的住户、国有农场的职工和农村个体工商户。位于乡镇（不包括城关镇）行政管理区域内和在城关镇所辖行政村范围内的国有经济的机关、团体、学校、企事业单位的集体户不属于农户。<br />
+</span>','<span style="font-size:16px;"> </span><span style="font-size:16px;">“致富通”农户贷款系列是指农村信用社（农村商业银行）以评定辖区内农户信用等级为基础，向符合条件的农户发放的用于生产经营、生活消费等用途的贷款。<br />
+农户是指长期(一年以上)居住在乡镇和城关镇所辖行政村的住户和户口不在本地而在本地居住一年以上的住户、国有农场的职工和农村个体工商户。位于乡镇（不包括城关镇）行政管理区域内和在城关镇所辖行政村范围内的国有经济的机关、团体、学校、企事业单位的集体户不属于农户。<br />
+</span><span style="font-size:16px;"></span>','资金循环使用、贷款随借随还、贷款流程简化、特色支农服务','124808',null,'0','16,21,22,17,20,18,19,25,24,23','16','<span style="font-size:16px;"> 
+<p>
+	<span style="font-size:16px;">居住在信用社服务辖区内，经农村信用社（农村商业银行）评级授信后的信用农户</span> 
+</p>
+<p>
+	<span style="font-size:16px;">1. &nbsp; 农户贷款以户为单位申请发放，并明确一名家庭成员为借款人，借款人应至少在18周岁（含）至65周岁（含）之间，具有完全民事行为能力和劳动能力、持有有效身份证件的中华人民共和国公民；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2. &nbsp; 贷款人评定的信用农户，户籍所在地、自有固定住所或固定经营场所在贷款人的服务辖区内；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3. &nbsp; 所从事的生产经营活动符合法律法规及产业政策；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4. &nbsp; 贷款申请数额、期限合理，符合生产经营活动、消费行为的合理需求；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5. &nbsp; 具有按期偿还贷款本息的能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">6. &nbsp; 借款人无恶意逃废债行为，无重大不良信用记录，遵纪守法，品行良好；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">7. &nbsp; 在贷款人处开立个人银行结算账户，并通过该账户发放贷款，自愿接受信贷监督和支付结算监督；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">8. &nbsp; 非信用农户的信贷产品，借款条件按照该信贷产品的规定；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">9. &nbsp; 贷款人要求的其他条件。</span> 
+</p>
+</span>','<p>
+	<span style="font-size:16px;">居住在信用社服务辖区内，经农村信用社（农村商业银行）评级授信后的信用农户</span> 
+</p>
+<p>
+	<span style="font-size:16px;">1. &nbsp; 农户贷款以户为单位申请发放，并明确一名家庭成员为借款人，借款人应至少在18周岁（含）至65周岁（含）之间，具有完全民事行为能力和劳动能力、持有有效身份证件的中华人民共和国公民；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2. &nbsp; 贷款人评定的信用农户，户籍所在地、自有固定住所或固定经营场所在贷款人的服务辖区内；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3. &nbsp; 所从事的生产经营活动符合法律法规及产业政策；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4. &nbsp; 贷款申请数额、期限合理，符合生产经营活动、消费行为的合理需求；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5. &nbsp; 具有按期偿还贷款本息的能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">6. &nbsp; 借款人无恶意逃废债行为，无重大不良信用记录，遵纪守法，品行良好；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">7. &nbsp; 在贷款人处开立个人银行结算账户，并通过该账户发放贷款，自愿接受信贷监督和支付结算监督；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">8. &nbsp; 非信用农户的信贷产品，借款条件按照该信贷产品的规定；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">9. &nbsp; 贷款人要求的其他条件。</span> 
+</p>','<p>
+	<span style="font-size:16px;"><span style="font-size:16px;">1. 服务辖区内网点为客户建档评级授信</span> ；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2. 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">3. 客户登陆黔农e贷PC端或移动端APP进行注册、认证</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">4. 客户到服务网点办理黔农e贷签约</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">5. 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务</span> 。
+</p>','<p>
+	<span style="font-size:16px;">1.&nbsp; 服务辖区内网点为客户建档评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2.&nbsp; 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">3.&nbsp; 客户登陆黔农e贷PC端或移动端APP进行注册、认证</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">4.&nbsp; 客户到服务网点办理黔农e贷签约</span> ；
+</p>
+<p>
+	<span style="font-size:16px;">5.&nbsp; 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务</span> 。
+</p>','<span style="font-size:16px;"><span style="font-size:16px;"> 资金循环使用、贷款随借随还、贷款流程简化、特色支农服务</span></span><span style="font-size:16px;"></span>','<span style="font-size:16px;"> 资金循环使用、贷款随借随还、贷款流程简化、特色支农服务</span><span style="font-size:16px;"></span>','232010000',to_timestamp('21-4月 -17 09.33.55.000000000 上午','DD-MON-RR HH.MI.SSXFF AM'),'124808',to_timestamp('28-5月 -17 08.38.05.000000000 下午','DD-MON-RR HH.MI.SSXFF AM'),500000,'3',1);
+Insert into CMS_PRD_CAT (ID,OU,STATUS,CRD_CD,CRD_NM,IMG_PC,IMG_APP,IMG_PC_DETAIL,IMG_APP_DETAIL,DETAILS_PC,DETAILS_APP,DESCRIPTION,RELEASED_BY,RELEASED_COMMENTS,PRIORITY,USES,USES_DEFAULT,COND_APP,COND_PC,PROC_APP,PROC_PC,FTR_APP,FTR_PC,CREATORS_ID,CREATE_TIMESTAMP,MODIFIERS_ID,MODIFY_TIMESTAMP,AMOUNT,CTS,SORT) values (100002,'999','4',null,'薪动力','/2017-04/21/c92b21cd1a4041cbb46dcdd1783f48a1.jpg','/2017-04/21/035c6adf0b414befa03cd7b3f0cdfb08.jpg','/2017-04/21/945ad2d42b93405bb74c5e4ac5f8b3e9.jpg','/2017-04/21/7c14c9ac2e8d46d8a03fc803dcd5af35.jpg','<p>
+	<span style="font-size:16px;"></span>
+</p>
+<p>
+	<span style="font-size:16px;">“薪动力”行政企事业单位职工消费贷款是指农村信用社（农村商业银行）以评定辖区域内国家行政企事业单位正式在编在岗职工信用等级为基础，向工资收入稳定，具有完全民事行为能力的自然人发放的，用于房屋装修、购车、大额耐用消费品、旅游、医疗及子女就学等，以工资收入为主要还款来源的综合消费贷款。</span> 
+</p>','<p>
+	<span style="font-size:16px;"></span>
+</p>
+<p>
+	<span style="font-size:16px;">“薪动力”行政企事业单位职工消费贷款是指农村信用社（农村商业银行）以评定辖区域内国家行政企事业单位正式在编在岗职工信用等级为基础，向工资收入稳定，具有完全民事行为能力的自然人发放的，用于房屋装修、购车、大额耐用消费品、旅游、医疗及子女就学等，以工资收入为主要还款来源的综合消费贷款。</span> 
+</p>','资金循环使用、贷款随借随还、贷款流程简化、覆盖家庭消费','100247',null,'0','27,33,29,30,31,32,26,28','27','<p>
+	<span style="font-size:16px;">在农村信用社（农村商业银行）所辖区域范围内的国家行政企事业单位正式在编在岗职工</span> 
+</p>
+<p>
+	<span style="font-size:16px;">1.&nbsp; 思想品德好，诚实守信，无不良嗜好；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2.&nbsp; 信用状况良好，无重大不良记录；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3.&nbsp; 有稳定的收入来源，能按期还本付息；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4.&nbsp; 信用等级评定达到特优、优秀、较好、一般标准；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5.&nbsp; 贷款人对单位职工进行以个人工资评级授信或以家庭综合评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">6.&nbsp; 贷款人要求的其他条件。</span> 
+</p>','<p>
+	<span style="font-size:16px;">在农村信用社（农村商业银行）所辖区域范围内的国家行政企事业单位正式在编在岗职工</span> 
+</p>
+<p>
+	<span style="font-size:16px;">1.&nbsp; 思想品德好，诚实守信，无不良嗜好；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2.&nbsp; 信用状况良好，无重大不良记录；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3.&nbsp; 有稳定的收入来源，能按期还本付息；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4.&nbsp; 信用等级评定达到特优、优秀、较好、一般标准；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5.&nbsp; 贷款人对单位职工进行以个人工资评级授信或以家庭综合评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">6.&nbsp; 贷款人要求的其他条件。</span> 
+</p>','<p>
+	<span style="font-size:16px;">1.&nbsp; 服务辖区内网点为客户建档评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2.&nbsp; 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3.&nbsp; 客户登陆黔农e贷PC端或移动端APP进行注册、认证；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4.&nbsp; 客户到服务网点办理黔农e贷签约；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5.&nbsp; 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务。</span> 
+</p>','<p>
+	<span style="font-size:16px;">1.&nbsp; 服务辖区内网点为客户建档评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">2.&nbsp; 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">3.&nbsp; 客户登陆黔农e贷PC端或移动端APP进行注册、认证；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">4.&nbsp; 客户到服务网点办理黔农e贷签约；</span> 
+</p>
+<p>
+	<span style="font-size:16px;">5.&nbsp; 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务。</span> 
+</p>','<span style="font-size:16px;">资金循环使用、贷款随借随还、贷款流程简化、覆盖家庭消费</span>','<span style="font-size:16px;">资金循环使用、贷款随借随还、贷款流程简化、覆盖家庭消费</span><span style="font-size:16px;"></span>','232010000',to_timestamp('21-4月 -17 09.31.20.000000000 上午','DD-MON-RR HH.MI.SSXFF AM'),'100247',to_timestamp('01-12月-17 05.59.21.000000000 下午','DD-MON-RR HH.MI.SSXFF AM'),500000,'6',2);
+Insert into CMS_PRD_CAT (ID,OU,STATUS,CRD_CD,CRD_NM,IMG_PC,IMG_APP,IMG_PC_DETAIL,IMG_APP_DETAIL,DETAILS_PC,DETAILS_APP,DESCRIPTION,RELEASED_BY,RELEASED_COMMENTS,PRIORITY,USES,USES_DEFAULT,COND_APP,COND_PC,PROC_APP,PROC_PC,FTR_APP,FTR_PC,CREATORS_ID,CREATE_TIMESTAMP,MODIFIERS_ID,MODIFY_TIMESTAMP,AMOUNT,CTS,SORT) values (100003,'999','4',null,'个体户','/2017-04/21/ca8bc4533f164528bd5dce75d0c7974a.jpg','/2017-04/21/836851a4cb7c4f6cbdcea529ab6191f6.jpg','/2017-04/21/c1fbbea1c8754f5bbe19073c34f4cc8f.jpg','/2017-04/21/b349a2e0c3494091bcae35a8a0357d09.jpg','<span style="font-size:16px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 个体经营户贷款是指农村信用社（农村商业银行）以评定辖区域内从事合法生产经营活动的自然人信用等级为基础,满足生产经营所需发放的贷款；其中评定对象包括个体工商户、私营业主、个人独资企业投资人和非法人资格私营企业的承包人或承租人等。</span>','<span style="font-size:16px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 个体经营户贷款是指农村信用社（农村商业银行）以评定辖区域内从事合法生产经营活动的自然人信用等级为基础,满足生产经营所需发放的贷款；其中评定对象包括个体工商户、私营业主、个人独资企业投资人和非法人资格私营企业的承包人或承租人等。</span>','资金周转使用、贷款随借随还、贷款流程简化、满足生产经营','100247',null,'0','37,39,40,41,34,35,36,38','37','<p>
+	<span style="font-size:16px;">在农村信用社（农村商业银行）服务辖区域内的个体经营户</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">1.&nbsp; 年满18周岁（含）至65周岁（含）之间，具有完全民事行为能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">2.&nbsp; 申请人具有合法的居住证明，并有固定的住所和相对固定的经营场所；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">3.&nbsp; 有稳定、合法的收入来源，有按期偿还贷款本息的能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">4.&nbsp; 遵纪守法，诚实守信，无重大不良信用记录；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">5.&nbsp; 信用等级评定达到特优、优秀、较好、一般标准</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">6.&nbsp; 从事合法的生产经营活动；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">7.&nbsp; 贷款人要求的其他条件。</span> 
+</p>','<p>
+	<span style="font-size:16px;">在农村信用社（农村商业银行）服务辖区域内的个体经营户</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">1.&nbsp; 年满18周岁（含）至65周岁（含）之间，具有完全民事行为能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">2.&nbsp; 申请人具有合法的居住证明，并有固定的住所和相对固定的经营场所；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">3.&nbsp; 有稳定、合法的收入来源，有按期偿还贷款本息的能力；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">4.&nbsp; 遵纪守法，诚实守信，无重大不良信用记录；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">5.&nbsp; 信用等级评定达到特优、优秀、较好、一般标准</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">6.&nbsp; 从事合法的生产经营活动；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">7.&nbsp; 贷款人要求的其他条件。</span> 
+</p>','<p>
+	<span style="font-size:16px;">1.&nbsp; 服务辖区内网点为客户建档评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">2.&nbsp; 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">3.&nbsp; 客户登陆黔农e贷PC端或移动端APP进行注册、认证；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">4.&nbsp; 客户到服务网点办理黔农e贷签约；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">5.&nbsp; 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务</span> 
+</p>','<p>
+	<span style="font-size:16px;">1.&nbsp; 服务辖区内网点为客户建档评级授信；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">2.&nbsp; 客户持授信通知书或贷款证（卡）到服务辖区内网点提出借款申请；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">3.&nbsp; 客户登陆黔农e贷PC端或移动端APP进行注册、认证；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">4.&nbsp; 客户到服务网点办理黔农e贷签约；</span> 
+</p>
+<p>
+	<span style="font-size:16px;"></span><span style="font-size:16px;">5.&nbsp; 客户到黔农e贷平台自助办理查询、借款、还款、预约等业务</span> 
+</p>','<span style="font-size:16px;">资金周转使用、贷款随借</span><span style="font-size:16px;">随还、贷款流程简化、满足生产经营</span>','<span style="font-size:16px;">资金周转使用、贷款随借随还、贷款流程简化、满足生产经营</span>','232010000',to_timestamp('21-4月 -17 09.24.03.000000000 上午','DD-MON-RR HH.MI.SSXFF AM'),'100247',to_timestamp('01-12月-17 05.59.50.000000000 下午','DD-MON-RR HH.MI.SSXFF AM'),1000000,'4',3);
